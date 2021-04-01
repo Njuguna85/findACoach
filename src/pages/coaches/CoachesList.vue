c66ffe9f2f1f794c4898d6da3442c56ee22be414
Njuguna85 <dennix.njuguna85@gmail.com>
2021-04-01 12:25:13 +0300

    add routes and state mgmt

---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>FILTER</section>
+  <section>
+    <div class="controls">
+      <button @click="logst">Refresh</button>
+      <router-link to="/register">Register as a coach</router-link>
+    </div>
+    <ul>
+      <li v-for="coach in filteredCoaches" :key="coach.id">
+        {{ coach.firstName }}
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+  },
+  methods: {
+    logst() {
+      console.log(this.$store.state.coaches);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
