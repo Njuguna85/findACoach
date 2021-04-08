2d548cafca8aa5a278829dfcf6fe3a771e8f0211
Njuguna85 <dennix.njuguna85@gmail.com>
2021-04-08 09:59:12 +0300

    add backend

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,7 +1,7 @@
 export default {
-    registerCoach(context, data) {
+    async registerCoach(context, data) {
+        const userId = context.rootGetters.userId
         const coachData = {
-            id: context.rootGetters.userId,
             firstName: data.first,
             lastName: data.last,
             hourlyRate: data.rate,
@@ -9,6 +9,47 @@ export default {
             areas: data.areas
 
         }
-        context.commit('registerCoach', coachData);
+
+        const response = await fetch(`https://vue-http-demo-1a915-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+
+        // const responseData = await response.json();
+
+        if (!response.ok) {
+            // error
+        }
+
+        context.commit('registerCoach', {
+            ...coachData,
+            id: userId
+        });
+    },
+    async loadCoaches(context) {
+        let response = await fetch(`https://vue-http-demo-1a915-default-rtdb.firebaseio.com/coaches.json`);
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            // error
+        }
+
+        const coaches = [];
+
+        for (const key in responseData) {
+            const coach = {
+                id: key,
+                firstName: responseData[key].firstName,
+                lastName: responseData[key].lastName,
+                hourlyRate: responseData[key].hourlyRate,
+                description: responseData[key].description,
+                areas: responseData[key].areas
+            }
+            coaches.push(coach);
+        }
+
+        context.commit('setCoaches', coaches)
+
     }
 }
